4297d59483967bd6c3a76e10050e555dc229d16c
Natasha <nhodg2@gmail.com>
2023-10-28 20:21:27 -0500

    Update Employer model class with a location field, Skill model class with a description field, and both classes with Entity annotation, public accessor methods, and  no-arg constructor

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "There must be a location.")
+    @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters")
+    private String location;
+
+    public Employer () {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
