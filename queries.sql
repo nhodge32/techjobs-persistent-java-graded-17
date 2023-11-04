fcf32d4f7ebeaa4dfeea5153c9c562a9c8be24c0
Natasha <nhodg2@gmail.com>
2023-11-03 20:46:43 -0500

    Update queries.sql with Part 1 comments listing the job columns and data types

--- queries.sql
+++ queries.sql
@@ -1,6 +1,13 @@
 --Part 1
+--Job columns (data type)
+--id (int)
+--employer (String)
+--name (String)
+--skills (String)
 
 --Part 2
+--list the names of the employers in St. Louis City
+
 
 --Part 3
 
