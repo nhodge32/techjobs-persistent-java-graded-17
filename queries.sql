8de6a4c16492364a59d5a05d0cc00ab209aae2a9
Natasha <nhodg2@gmail.com>
2023-11-13 17:39:13 -0600

    Update queries.sql to query all the skills listed in alphabetical order that have a job attached to it

--- queries.sql
+++ queries.sql
@@ -13,3 +13,7 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 
 --Part 4
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
